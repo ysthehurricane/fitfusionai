8d979ab8006df81c1397edc5cc68323859727471
Yash <Patelyash2504@gmail.com>
2025-09-05 13:40:55 +0530

    update css conf is setting

--- gymer/settings.py
+++ gymer/settings.py
@@ -156,10 +156,17 @@ USE_TZ = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 BASE_URL = "http://yash-intern.ybaisolution.com/"
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
